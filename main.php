2c6ae113de8afd8d8c768b686c74ca07d7c73546
jmsangar <jmansangar@gmail.com>
2014-04-05 13:10:53 +0200

    trato de subir esto
    nprimera subida en master

---
+++ main.php
@@ -0,0 +1,24 @@
+<main>
+		
+	<?php the_post(); ?>
+ 
+        <article>
+ 			<header>
+ 			<!-- Aqui saco el titulo del post -->
+            <h1><?php the_title(); ?></h1>
+			<!-- Aqui saco el autor y la fecha de publicacion del post -->
+                <small>
+                    Por <?php the_author(', '); ?> el <?php the_time('F jS, Y'); ?> en <?php the_category(', ') ?> | <?php comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?>
+                </small>
+         	</header>       
+ 			<!-- Aqui el contenido del post -->
+			<div class="single-content">
+                <?php the_content(); ?>
+            </div>
+
+
+ 
+        </article>
+
+
+</main>
---
+++ page.php
@@ -0,0 +1,11 @@
+<?php get_header(page); ?>
+
+
+	
+<?php get_template_part("the_loop"); ?>
+
+
+
+
+<?php get_footer(); ?>
+
